859ebb9557a64a2e5f463a71084f7acac51dcbb6
SDO_MF <m.faehnle@sedevo.de>
2025-07-08 10:21:25 +0200

    added randome slices

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
@@ -10,6 +11,23 @@ func Hello(name string) (string, error) {
 		return "", errors.New("empty name")
 	}
 
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+
+	// a slice of message formats
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Go get me a beer, %v!",
+		"Do I have to ask u twice? Go get me a damn beer, %v.",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
+
 }
